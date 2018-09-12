eea0f67ae122706be05ff891c5c59a56d1cf26e9
Pierre Macedo <pierre.alexander.mg@gmail.com>
2018-09-12 19:50:48 -0300

    fix

--- temp-key-storage-common.js
+++ temp-key-storage-common.js
@@ -1,16 +1,19 @@
-let Sqlite = require("nativescript-sqlcipher");
-let CryptoJS = require("crypto-js");
+const Sqlite = require("nativescript-sqlcipher");
+const CryptoJS = require("crypto-js");
+
+let tempKey = "";
 
 function keyStorage(key, length) {
 
 let randomKey = {key: CryptoJS.lib.WordArray.random(32).toString(CryptoJS.enc.Hex)};
 let setLength = length;    
 
- if (Sqlite.exists("temp_storage")) {
-    Sqlite.deleteDatabase("temp_storage");
+    
+ if (Sqlite.exists("temp_storage.db")) {
+    Sqlite.deleteDatabase("temp_storage.db");
     }
 
-   new Sqlite("temp_storage", randomKey).then(db =>{
+   new Sqlite("temp_storage.db", randomKey).then(db =>{
       db.execSQL('CREATE TABLE IF NOT EXISTS key (id INTEGER PRIMARY KEY AUTOINCREMENT, main_key TEXT)');
       db.execSQL("INSERT INTO key (main_key) VALUES (?)", [key]).then(id => {
         reKey(randomKey, setLength);  
@@ -19,38 +22,42 @@ let setLength = length;
 }
 
 function reKey(arg, length) {
-    global.tempKey = arg;  
+    tempKey = arg;  
     let setLength = length;
     
+        
      setTimeout(function(){
     let random = CryptoJS.lib.WordArray.random(32).toString(CryptoJS.enc.Hex);  
     let rekeyDb = {key: random};        
-      new Sqlite("temp_storage", tempKey).then(db =>{           
+      new Sqlite("temp_storage.db", tempKey).then(db =>{           
       db.execSQL("PRAGMA rekey = '" + random + "';");
           console.log("ok");
       }).then(function() {reKey(rekeyDb, setLength);});     
     }, length);
+    
    
     }
     
     function processDb(){
 
      let mainKey = "";    
-    this.getKey = function(){  
-    new Sqlite("temp_storage", tempKey).then(db =>{    
+    this.getKey = function(){
+      
+    new Sqlite("temp_storage.db", tempKey).then(db =>{    
      db.all("SELECT * FROM key").then(rows => {
             for(var row in rows) {
                 mainKey = rows[row][1]; 
             }
         });
     });  
+    
     }
      this.returnKey = function() {
          return mainKey;
      }    
     
     this.deleteDb = function() {
-    Sqlite.deleteDatabase("temp_storage");
+    Sqlite.deleteDatabase("temp_storage.db");
     }
     }
 
